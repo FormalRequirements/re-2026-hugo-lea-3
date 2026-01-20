9ca0e957b7268967eff0d7273ec35001ac84c752
Hugo GONCALVES <104032491+hugoglvs@users.noreply.github.com>
2026-01-20 16:43:31 +0100

    feat(docs): implement tag-based automated revision history

--- scripts/generate_changelog.rb
+++ scripts/generate_changelog.rb
@@ -4,84 +4,41 @@
 OUTPUT_FILE = 'docs/generated/changelog.adoc'
 REPO_DIR = '.'
 
-def get_commits
-  # Get commits in format: hash|date|author|subject
-  # We limit to last 50 for this example, but could be range-based
-  log = `git log -n 50 --pretty=format:"%h|%ad|%an|%s" --date=short`
-  log.split("\n")
+def get_tags
+  # Get tags in format: tag|date|author|subject
+  # We use raw git format. Note: 'taggername' works for annotated tags. 
+  # For lightweight tags, we might need a fallback or just use committer.
+  # We'll use %(subject) for the message (first line of annotation or commit message).
+  format = "%(refname:short)|%(creatordate:short)|%(taggername)|%(subject)"
+  tags = `git tag -l --format='#{format}' --sort=-creatordate`
+  tags.split("\n")
 end
 
-def parse_commit(line)
-  hash, date, author, subject = line.split('|')
-  
-  # Conventional Commits Regex
-  # type(scope)!: description
-  match = subject.match(/^(\w+)(?:\(([^)]+)\))?(!?):\s+(.*)$/)
-  
-  if match
-    {
-      hash: hash,
-      date: date,
-      author: author,
-      type: match[1],
-      scope: match[2],
-      breaking: match[3] == '!',
-      description: match[4]
-    }
-  else
-    # Non-conventional fallback
-    {
-      hash: hash,
-      date: date,
-      author: author,
-      type: 'other',
-      scope: nil,
-      breaking: false,
-      description: subject
-    }
-  end
-end
-
-commits = get_commits.map { |line| parse_commit(line) }
-
-# Group by Type
-grouped = commits.group_by { |c| c[:type] }
-
-# Order of types to display
-TYPE_ORDER = {
-  'feat' => 'Features',
-  'fix' => 'Bug Fixes',
-  'docs' => 'Documentation',
-  'style' => 'Styles',
-  'refactor' => 'Code Refactoring',
-  'perf' => 'Performance Improvements',
-  'test' => 'Tests',
-  'build' => 'Builds',
-  'ci' => 'CI',
-  'chore' => 'Chores',
-  'revert' => 'Reverts',
-  'other' => 'Other Changes'
-}
+tags = get_tags
 
 File.open(OUTPUT_FILE, 'w') do |f|
-  f.puts "= Changelog"
-  f.puts ":toc:"
-  f.puts
-  f.puts "Generated from git history at #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}"
-  f.puts
+  # We don't output a main title here because this file is intended 
+  # to be included inside the table in intro.adoc
   
-  TYPE_ORDER.each do |type, label|
-    next unless grouped[type] && !grouped[type].empty?
+  # If we want to generate the WHOLE table, we do:
+  f.puts "[cols=\"1,1,2,3\", options=\"header\"]"
+  f.puts "|==="
+  f.puts "| Version | Date | Author | Description"
+  
+  tags.each do |line|
+    version, date, author, subject = line.split('|')
+    author = "Unknown" if author.to_s.strip.empty?
     
-    f.puts "== #{label}"
-    f.puts
-    grouped[type].each do |commit|
-      scope = commit[:scope] ? "*#{commit[:scope]}:* " : ""
-      breaking = commit[:breaking] ? " [BREAKING]" : ""
-      f.puts "* #{commit[:date]} - #{scope}#{commit[:description]}#{breaking} ({commit[:hash]})"
-    end
-    f.puts
+    # Clean up data
+    version.strip!
+    date.strip!
+    author.strip!
+    subject.strip!
+    
+    f.puts "| #{version} | #{date} | #{author} | #{subject}"
   end
+  
+  f.puts "|==="
 end
 
 puts "Generated #{OUTPUT_FILE}"
